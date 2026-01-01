8e0f7f041dbce1fc8b209be5c9a550a4780a0cfe
Silviu Miron <silviu_miron_90@outlook.com>
2026-01-01 21:10:54 +0000

    Initial commit

---
+++ exporter.py
@@ -0,0 +1,6 @@
+
+"""
+This module is responsible to  demonstrating the OOP (Object-Oriented Program). The data output information about each park, including numbers of reviews, numbers of positive reviews.
+The output provide option for TXT, CSV, and JSON formats.
+
+"""
---
+++ process.py
@@ -0,0 +1,7 @@
+"""
+This module is responsible for processing the data.  It will largely contain functions that will receive the overall dataset and
+perform necessary processes in order to provide the desired result in the desired format.
+It is likely that most sections will require functions to be placed in this module.
+"""
+
+
